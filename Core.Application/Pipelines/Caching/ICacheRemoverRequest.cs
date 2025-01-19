8f3f812f4258a3eed73ae3c0633b6ed4227271fb
Mustafa Kurt <mstfkurt07@gmail.com>
2025-01-19 18:10:56 +0300

    Caching

---
+++ Core.Application/Pipelines/Caching/ICacheRemoverRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+
+namespace Core.Application.Pipelines.Caching;
+
+public interface ICacheRemoverRequest
+{
+    string? CacheKey { get; }
+    bool BypassCache { get; }
+    string? CacheGroupKey { get; }
+}
